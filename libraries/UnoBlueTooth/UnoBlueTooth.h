7c683c7461a3d9362eab31f4144160bd7cd28997
van-vu-mq <36938569+van-vu-mq@users.noreply.github.com>
2018-10-05 10:24:29 +1000

    Add AT command functions
    Additional comments
    void changeName(String name);
    void changeRole(int role);

--- libraries/UnoBlueTooth/UnoBlueTooth.h
+++ libraries/UnoBlueTooth/UnoBlueTooth.h
@@ -12,39 +12,49 @@
 class UnoBlueTooth {
 	// Functions that are accessible by other programs.
 	public:
+		// initialize
 		UnoBlueTooth();
-		
 		void begin(int baudRate);
-		boolean getConnectionStatus();	
 		void connect();
 		
+		// communications
 		boolean sendData(String data);
 		boolean sendArray(String data[]);
 		String receiveData();		
 		
+		// settings and status
+		void changeName(String name);
+		void changeRole(int role);		
+		int getConnectionStatus();		
+		
 		// Testing functions. These are used to test concepts.
 		// Delete these afterwards to conserve system memory.
-		void readFromSerial();
+		void readFromSerialToBT();
 		void readFromBlueTooth();
-		void sendToBlueTooth();
-		void processArray(String dataSet[], int arraySize);
+		void readArray(String dataSet[], int arraySize);
 	
 	// Functions that can only be accessed internally by the Uno BlueTooth library.
 	private:
+		// globals
 		AltSoftSerial BTSerial;
 		int connectionStatusPin;
 		String MegaMAC;
 		
+		// security
 		String encrypt(String data);
 		String decrypt(String data);
 		
+		// transmit utility for communication
 		String addMarker(String data);
 		String removeMarker(String data);
 		
+		// error detection / correction
 		byte addCheckSum(byte data);
 		boolean confirmCheckSum(byte data);
 		
+		// uncategorised
 		void doATCommandSetup();
+		String atResponse();
 };
 
 #endif
