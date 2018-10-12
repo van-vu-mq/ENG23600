b26b2c4ad3fbcd22c4b1f8283340320c137affab
van-vu-mq <36938569+van-vu-mq@users.noreply.github.com>
2018-10-12 16:39:06 +1100

    Update function descriptions under initialisation and settings

--- UnoTestFrameWork/UnoBlueTooth.ino
+++ UnoTestFrameWork/UnoBlueTooth.ino
@@ -164,7 +164,7 @@ boolean isATSucessfull(String response, String successFlags[], int numFlags) {
 
 
 /*
-  @desc Listens on the AltSoftSerial ports for a response. Reads response into a string.
+  @desc Listen on the Serial port for a response, reads it and returns it as a single string.
   @param
   @return String - response
 */
@@ -193,318 +193,316 @@ String atResponse() {
     char c = BTSerial.read();
     response.concat(c);
   }
-
-
   Serial.println("\n" + response);
   return response;
 }
 
 /*
-  @desc
+  @desc Checks whether the conditions are met to execute AT commands
   @param
-  @return
+  @return boolean - false if not able to execute AT commands
+  @return boolean - true if able to execute AT commands
 */
 boolean canDoAT() {
-  if (getConnectionStatus()) {
+  if (!getConnectionStatus()) {
     return true;
   } else {
     Serial.println("Error.\nBlueTooth is currently paired, unable to perform AT commands");
     return false;
   }
-}
 
 
-/************************************************************************************************************************/
-/************************/
-/*      Transmit        */
-/************************/
-/************************************************************************************************************************/
+  /************************************************************************************************************************/
+  /************************/
+  /*      Transmit        */
+  /************************/
+  /************************************************************************************************************************/
 
-/*
-  @desc Handles the tranmission process for an array of data (String)
-  @param String data[] - array of message to be sent
-  @return boolean - success  or failure to send or be recieved by paired device.
-*/
-boolean sendData(String data[]) {
-  // TODO /*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/
+  /*
+    @desc Handles the tranmission process for an array of data (String)
+    @param String data[] - array of message to be sent
+    @return boolean - success  or failure to send or be recieved by paired device.
+  */
+  boolean sendData(String data[]) {
+    // TODO /*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/
 
-  // Add markers
+    // Add markers
 
-  // Convert to single String
+    // Convert to single String
 
-  // Encrypt string
+    // Encrypt string
 
-  // Add checksum to string
+    // Add checksum to string
 
-  // Wrap whole string in marker : '<', '>'
+    // Wrap whole string in marker : '<', '>'
 
-  // Write to BTSerial
+    // Write to BTSerial
 
-  // Listen for acknowlegde
+    // Listen for acknowlegde
 
-  // if (acknowledge received) {
-  //   return true;
-  // } else {
-  //   return false;
-  // }
+    // if (acknowledge received) {
+    //   return true;
+    // } else {
+    //   return false;
+    // }
 
-  return true;
-}
-
-/*
-  @desc Encrypts a given string.
-  @param String data
-  @return String - the data in encrypted form
-*/
-String encrypt(String data) {
-  // TODO /*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/
-  return data;
-}
-
-/*
-  @desc Add markers to delimite end/beginning of file and lines
-  @param String data
-  @return String - data with added markers
-*/
-void addMarker(String *dataArray, int arraySize) {
-  // TODO /*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/
-
-  // Add marker to every line in the array
-  // markers: startData, endData, startLine, endLine
-  //char markers = {'!', '@', '#', '$'};
-}
-
-/*
-  @desc Add extra bits to the data to be used for error checking after transmission
-  @param byte data - encrypted data in byte format
-  @return byte - data with checksum bits
-*/
-String addCheckSum(String data) {
-  // TODO /*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/
-
-  // calculate checksum
-
-  // wrap checksum with marker: example '&', '*'
-
-  // append checksum to data String
-
-  //return data with the checksum included
-
-  return "";
-
-}
-
-
-/************************************************************************************************************************/
-/************************/
-/*     Receive          */
-/************************/
-
-/************************************************************************************************************************/
-
-/*
-  @desc Handles incoming data from the BTLE and the necessary processes before passing the data to the next destination.
-  @param
-  @return pointer to string of the data after processing
-*/
-boolean receiveData() {
-  // TODO /*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/
-
-  // Check BTSerial if there is data
-  // timeout if no data for a set amount of time
-  //return false
-
-  // Read and ignore until you find the packetStart marker
-  // Read and store until you find the packetEnd marker
-  // timeout if too much time has passed and you have no new data before packetEnd marker is found
-  //return false
-
-  // Remove packet Start/End marker
-
-  // confirm checksum
-  // if (checksum is wrong) {
-  //   send error message to other bluetooth
-  // } else {
-  // send acknowledge to other bluetooth
-  // remove checksum
-  // decrypt
-  // rebuild into an array
-  // remove markers
-  // depending on requirement from other team
-  // we either return a pointer, a string, fill THEIR array with the data
-  // return true
-
-  // }
-
-  char c;
-  String data = "";
-  if (BTSerial.available()) {
-    c = BTSerial.read();
-    data.concat(c);
+    return true;
   }
 
-  return true;
-}
-
-/*
-  @desc Undoes the encryption on given String
-  @param String data - encrypted datas
-  @return String - unencrypted data
-*/
-String decrypt(String data) {
-  // TODO /*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/
-  return data;
-}
-
-/*
-  @desc Reads the checksum of given data
-  @param byte data
-  @return boolean - TRUE if data and checksum matches
-  @return boolean - FALSE if data and checksum does not match
-*/
-boolean confirmCheckSum(String data) {
-  // TODO /*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/
-
-  // Read the data and extract the part that is the checksum using the checksum markers
-  // example markers start of checksum '&', end of checksum '*'
-
-  // Caclculate the checksum on the data
-
-  // Compare the extracted checksum and the calculated checksum
-
-  // if (both checksum is the same) {
-  //   return true
-  // } else {
-  //   return false
-  // }
-
-  return true;
-}
-
-/*
-  @desc Removes the added markers for transmission from given data
-  @param String data - data that contains markers
-  @return String - data without markers
-*/
-void removeMarker(String *dataArray, int arraySize, String markersToRemove[]) {
-  // TODO /*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/
-
-  // edit the array at the pointer
-
-  // for (every marker in the markerList) {
-  //   for (every line in the dataArray) {
-  //     remove the marker
-  // }
-
-}
-
-/************************************************************************************************************************/
-/************************/
-/*      Test            */
-/*    TO BE DELETED     */
-/************************/
-/************************************************************************************************************************/
-
-
-/*
-  @desc Reads input from Serial Monitor and transmit it through BlueTooth
-  @param
-  @return
-*/
-void readFromSerialToBT() {
-  char c;
-  if (Serial.available()) {
-    c = Serial.read();
-    BTSerial.print(c);
-    Serial.print(c);
+  /*
+    @desc Encrypts a given string.
+    @param String data
+    @return String - the data in encrypted form
+  */
+  String encrypt(String data) {
+    // TODO /*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/
+    return data;
   }
-}
 
-/*
-  @desc Reads data receieved from BTLE and prints it to Serial Monitor
-  @param
-  @return
-*/
-void readFromBlueTooth() {
-  char c;
-  if (BTSerial.available()) {
-    c = BTSerial.read();
-    Serial.write(c);
+  /*
+    @desc Add markers to delimite end/beginning of file and lines
+    @param String data
+    @return String - data with added markers
+  */
+  void addMarker(String * dataArray, int arraySize) {
+    // TODO /*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/
+
+    // Add marker to every line in the array
+    // markers: startData, endData, startLine, endLine
+    //char markers = {'!', '@', '#', '$'};
   }
-}
 
-/*
-  @desc Reads a static array and prints it to Serial Monitor.
-  This function is for testing purposes
-  @param String dataSet[]
-  @param arraySize - the array size to limit memory reading in case where a pointer is passed instead of an array
-  @return
-*/
-void readArray(String dataSet[], int arraySize) {
-  Serial.println("\nReceiving array test starting...");
-  for (int i = 0; i < arraySize; i++) {
-    String line = dataSet[i];
-    if (!line.equals(NULL)) {
-      Serial.println(String(i) + ": " + line);
-    } else {
-      Serial.println(String(i) + ": empty line in array");
+  /*
+    @desc Add extra bits to the data to be used for error checking after transmission
+    @param byte data - encrypted data in byte format
+    @return byte - data with checksum bits
+  */
+  String addCheckSum(String data) {
+    // TODO /*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/
+
+    // calculate checksum
+
+    // wrap checksum with marker: example '&', '*'
+
+    // append checksum to data String
+
+    //return data with the checksum included
+
+    return "";
+
+  }
+
+
+  /************************************************************************************************************************/
+  /************************/
+  /*     Receive          */
+  /************************/
+
+  /************************************************************************************************************************/
+
+  /*
+    @desc Handles incoming data from the BTLE and the necessary processes before passing the data to the next destination.
+    @param
+    @return pointer to string of the data after processing
+  */
+  boolean receiveData() {
+    // TODO /*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/
+
+    // Check BTSerial if there is data
+    // timeout if no data for a set amount of time
+    //return false
+
+    // Read and ignore until you find the packetStart marker
+    // Read and store until you find the packetEnd marker
+    // timeout if too much time has passed and you have no new data before packetEnd marker is found
+    //return false
+
+    // Remove packet Start/End marker
+
+    // confirm checksum
+    // if (checksum is wrong) {
+    //   send error message to other bluetooth
+    // } else {
+    // send acknowledge to other bluetooth
+    // remove checksum
+    // decrypt
+    // rebuild into an array
+    // remove markers
+    // depending on requirement from other team
+    // we either return a pointer, a string, fill THEIR array with the data
+    // return true
+
+    // }
+
+    char c;
+    String data = "";
+    if (BTSerial.available()) {
+      c = BTSerial.read();
+      data.concat(c);
+    }
+
+    return true;
+  }
+
+  /*
+    @desc Undoes the encryption on given String
+    @param String data - encrypted datas
+    @return String - unencrypted data
+  */
+  String decrypt(String data) {
+    // TODO /*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/
+    return data;
+  }
+
+  /*
+    @desc Reads the checksum of given data
+    @param byte data
+    @return boolean - TRUE if data and checksum matches
+    @return boolean - FALSE if data and checksum does not match
+  */
+  boolean confirmCheckSum(String data) {
+    // TODO /*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/
+
+    // Read the data and extract the part that is the checksum using the checksum markers
+    // example markers start of checksum '&', end of checksum '*'
+
+    // Caclculate the checksum on the data
+
+    // Compare the extracted checksum and the calculated checksum
+
+    // if (both checksum is the same) {
+    //   return true
+    // } else {
+    //   return false
+    // }
+
+    return true;
+  }
+
+  /*
+    @desc Removes the added markers for transmission from given data
+    @param String data - data that contains markers
+    @return String - data without markers
+  */
+  void removeMarker(String * dataArray, int arraySize, String markersToRemove[]) {
+    // TODO /*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/
+
+    // edit the array at the pointer
+
+    // for (every marker in the markerList) {
+    //   for (every line in the dataArray) {
+    //     remove the marker
+    // }
+
+  }
+
+  /************************************************************************************************************************/
+  /************************/
+  /*      Test            */
+  /*    TO BE DELETED     */
+  /************************/
+  /************************************************************************************************************************/
+
+
+  /*
+    @desc Reads input from Serial Monitor and transmit it through BlueTooth
+    @param
+    @return
+  */
+  void readFromSerialToBT() {
+    char c;
+    if (Serial.available()) {
+      c = Serial.read();
+      BTSerial.print(c);
+      Serial.print(c);
     }
   }
-  Serial.println("Receiving array test complete.");
 
-}
-
-/*
-  @desc Reads and prints out data of an array from given pointer
-  @param *data - pointer to the array
-  @param arraySize
-  @return
-*/
-void readArrayFromPointer(String *data, int arraySize) {
-  Serial.println("\nReceiving array pointer test starting...");
-  for (int i = 0; i < arraySize; i++) {
-    String line = *(data + i);
-    if (!line.equals(NULL)) {
-      Serial.println(String(i) + ": " + line);
-    } else {
-      Serial.println(String(i) + ": empty line in array");
+  /*
+    @desc Reads data receieved from BTLE and prints it to Serial Monitor
+    @param
+    @return
+  */
+  void readFromBlueTooth() {
+    char c;
+    if (BTSerial.available()) {
+      c = BTSerial.read();
+      Serial.write(c);
     }
   }
-  Serial.println("Receiving array pointer test complete.");
-}
 
-/*
-  @desc Calculate checksum for static data.
-  Example of how to use the CRC32 library
-  @param
-  @return
-*/
-void sampleCheckSum() {
-  // Checksum of "Hello World"
-  // Use to compare the function output.
-  const uint32_t KNOWN_CHECKSUM = 0x4A17B156;
+  /*
+    @desc Reads a static array and prints it to Serial Monitor.
+    This function is for testing purposes
+    @param String dataSet[]
+    @param arraySize - the array size to limit memory reading in case where a pointer is passed instead of an array
+    @return
+  */
+  void readArray(String dataSet[], int arraySize) {
+    Serial.println("\nReceiving array test starting...");
+    for (int i = 0; i < arraySize; i++) {
+      String line = dataSet[i];
+      if (!line.equals(NULL)) {
+        Serial.println(String(i) + ": " + line);
+      } else {
+        Serial.println(String(i) + ": empty line in array");
+      }
+    }
+    Serial.println("Receiving array test complete.");
 
-  // Data to calculate checksum
-  uint8_t byteBuffer[] = "Hello World";
-  size_t numBytes = sizeof(byteBuffer) - 1;
+  }
 
-  // Checksum library instance.
-  CRC32 crc;
+  /*
+    @desc Reads and prints out data of an array from given pointer
+    @param *data - pointer to the array
+    @param arraySize
+    @return
+  */
+  void readArrayFromPointer(String * data, int arraySize) {
+    Serial.println("\nReceiving array pointer test starting...");
+    for (int i = 0; i < arraySize; i++) {
+      String line = *(data + i);
+      if (!line.equals(NULL)) {
+        Serial.println(String(i) + ": " + line);
+      } else {
+        Serial.println(String(i) + ": empty line in array");
+      }
+    }
+    Serial.println("Receiving array pointer test complete.");
+  }
 
-  // store calculated checksum
-  uint32_t checksum;
+  /*
+    @desc Calculate checksum for static data.
+    Example of how to use the CRC32 library
+    @param
+    @return
+  */
+  void sampleCheckSum() {
+    // Checksum of "Hello World"
+    // Use to compare the function output.
+    const uint32_t KNOWN_CHECKSUM = 0x4A17B156;
 
-  // calculate checksum for the entire buffer at once
-  checksum = crc.calculate(byteBuffer, numBytes);
+    // Data to calculate checksum
+    uint8_t byteBuffer[] = "Hello World";
+    size_t numBytes = sizeof(byteBuffer) - 1;
 
-  // print out to compare
-  Serial.println("Known checksum:   " + String(KNOWN_CHECKSUM));
-  Serial.println("Generated checksum: " + String(checksum));
-  Serial.println("\nCheckSums as HEX");
-  Serial.println(KNOWN_CHECKSUM, HEX);
-  Serial.println(checksum, HEX);
+    // Checksum library instance.
+    CRC32 crc;
 
-}
+    // store calculated checksum
+    uint32_t checksum;
+
+    // calculate checksum for the entire buffer at once
+    checksum = crc.calculate(byteBuffer, numBytes);
+
+    // print out to compare
+    Serial.println("Known checksum:   " + String(KNOWN_CHECKSUM));
+    Serial.println("Generated checksum: " + String(checksum));
+    Serial.println("\nCheckSums as HEX");
+    Serial.println(KNOWN_CHECKSUM, HEX);
+    Serial.println(checksum, HEX);
+
+  }
 
 
 
