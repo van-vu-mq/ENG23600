13357a4c02e908491ef3a1bdebb2e3261bcc23cd
van-vu-mq <36938569+van-vu-mq@users.noreply.github.com>
2018-10-22 23:35:17 +1100

    Add function to simulate transmission errors / corrupted data

--- UnoTestFrameWork/SendTest.ino
+++ UnoTestFrameWork/SendTest.ino
@@ -1,11 +1,10 @@
-
+int maxCan = 10;
+int maxOneColour = 8;
 
 void testAllOrders() {
-  int maxCan = 10;
-  int maxOneColour = 8;
   int counter = 0;
   int testCount = 500;
-  
+
   // clear serial
   while (Serial.available()) {
     Serial.read();
@@ -27,5 +26,75 @@ void testAllOrders() {
       }
     }
   }
+}
+
+
+
+void sendCorruptData() {
+  int numOfFaults = randomValue(5); // how many times to insert simulated corruption into packet
+  int maxSizeOfFault = 5; // how many bytes the inserted corruption simulation should be
+  int testSampleSize = 100;
+
+  int arraySize = 4;
+  String sampleData[arraySize] = {"INT", "",  "", ""};
+
+  int sentCounter = 0;
+
+  while (sentCounter < testSampleSize) {
+    // generate random test data
+    int r = maxOneColour;
+    int g = maxOneColour;
+    int b = maxOneColour;
+
+    while ( r + g + b > maxCan) {
+      r = randomValue(maxOneColour);
+      g = randomValue(maxOneColour);
+      b = randomValue(maxOneColour);
+    }
+    sampleData[1] = r;
+    sampleData[2] = g;
+    sampleData[3] = b;
+
+
+
+    // Build packet for transmission as usual
+    // Assume that the function on the same board always works
+
+    addMarker(&sampleData[0], arraySize);
+
+    // Convert to single String
+    String packet = transformToString(sampleData, arraySize);
+
+    // Prepend checksum
+    packet = addCheckSum(packet);
+
+    // add packet markers
+    packet = "<" + packet + ">";
+
+
+    // add corruption
+    // simulates interference, bad pin connections, poor signal etc
+    for (int i = 0; i < numOfFaults; i++) {
+      int lengthOfFault = randomValue(maxSizeOfFault);
+      String randomData = randomString(lengthOfFault);
+      int posOfFault = randomValue(packet.length());
+
+      // insert random data
+      // left of pos + random data + right of pos
+      packet = packet.substring(0, posOfFault) + randomData + packet.substring(posOfFault);
+    }
+
+    // print to serial for logging
+    Serial.println(String(sentCounter) + ": " + packet);
+
+    // transmit the corrupt data via bluetooth
+    transmitData(packet);
+
+    sentCounter++;
+
+  }
+
+
+
 }
 
